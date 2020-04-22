bb6be722d5deaa4609fc658ce412a6a1d80e0b35
Jason Brown <cscarlsson@gmail.com>
2020-04-22 14:50:33 +0100

    Changes for Single Responsibility Principle

--- ArdalisRating.Tests/RatingEngineRate.cs
+++ ArdalisRating.Tests/RatingEngineRate.cs
@@ -7,24 +7,40 @@ namespace ArdalisRating.Tests
 {
     public class RatingEngineRate
     {
-[Fact]
-public void ReturnsRatingOf10000For200000LandPolicy()
-{
-    var policy = new Policy
-    {
-        Type = PolicyType.Land,
-        BondAmount = 200000,
-        Valuation = 200000
-    };
-    string json = JsonConvert.SerializeObject(policy);
-    File.WriteAllText("policy.json", json);
+        #region SRP Unit Tests
 
-    var engine = new RatingEngine();
-    engine.Rate();
-    var result = engine.Rating;
+        //[Fact]
+        //public void ReturnsDefaultPolicyFromEmptyJsonString()
+        //{
+        //    var inputJson = "{}";
+        //    var serializer = new JsonPolicySerializer();
 
-    Assert.Equal(10000, result);
-}
+        //    var result = serializer.GetPolicyFromJsonString(inputJson);
+
+        //    var policy = new Policy();
+        //    AssertPoliciesEqual(result, policy);
+        //}
+
+        #endregion
+
+        [Fact]
+        public void ReturnsRatingOf10000For200000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 200000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(10000, result);
+        }
 
         [Fact]
         public void ReturnsRatingOf0For200000BondOn260000LandPolicy()
---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string PolicyJson)
+        {
+            return JsonConvert.DeserializeObject<Policy>(PolicyJson,
+                new StringEnumConverter());
+        }
+    }
+}
